8e920609fded593938c03c550089c39e66c75e63
NataliyaShevchuk <nata.shevchuk@icloud.com>
2022-12-01 00:43:04 +0100

    esercizio completato

--- js/main.js
+++ js/main.js
@@ -11,14 +11,31 @@ createApp({
     // la funzione data deve SEMPRE ritornare un oggetto
     return {
         listaEmail: [],
+        counter: 0,
+        listaTemporanea: [],
     };
 },
 methods: {
     fetchData (){
         axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
-        .then((response) => {
-            this.listaEmail = response.data;
+        .then((resp) => {
+            console.log(resp); 
+
+            this.listaTemporanea.push(resp.data.response); //una stringa di email
+            
+            this.counter++;
+
+            if(this.counter === 10){
+                this.listaEmail = this.listaTemporanea; //this in questo caso equivale al value usato in js 
+                //in questo modo sto vedendo la stringa di lista Temporanea che rappresenta l'email
+            }
+
         });
     },
+    },
+    mounted() {
+        for (let i = 0; i <= 10; i++){
+            this.fetchData();
+        }
     }
 }).mount("#app")
